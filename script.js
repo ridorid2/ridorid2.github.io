ccb513a1979fce252ef6210ad9aa9a425cd918b1
ridorid2 <168337937+ridorid2@users.noreply.github.com>
2024-04-28 12:07:25 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const respo = document.querySelector(".respo")
+const navlinks = document.querySelector(".navlinks")
+
+respo.addEventListener('click',()=>{
+    navlinks.classList.toggle('mobile-menu')
+})
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,12 @@
+const btn1 = document .getElementById("btn1")
+const btn2 = document .getElementById("btn2")
+const btn3 = document .getElementById("btn3")
+const btn4 = document .getElementById("btn4")
+const btn5 = document .getElementById("btn5")
+
+
+onclick = getElementById("btn1") .src='pl.html' 
+
+
+
+
